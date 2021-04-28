8772c982405873c1f862841eec5a68c1dc092cea
Marco Wessel <marco@poop.nl>
2021-04-28 03:28:10 +0000

    use settings.json for stream url

--- app/index.js
+++ app/index.js
@@ -3,14 +3,14 @@ const devtools = require('choo-devtools')
 const css = require('sheetify')
 css('./design/index.js')
 css('./design/design.css')
+const settings = require('../public/settings.json')
 
 const app = choo()
 app.use(devtools())
 
 app.use(require('./stores/stream'))
 
-app.route('/ONaltplatforms', require('./views/main'))
-//app.route('/', require('./views/main'))
+app.route(settings.stream.url, require('./views/main'))
 app.route('*', require('./views/notfound'))
 
 if (!module.parent) app.mount('body')
