04e837321467d78725b319532b322aed30c6780c
Marco Wessel <marco@driebit.nl>
2021-11-16 13:01:57 +0100

    Add ignored domains to keep emotes out of the URL list

--- app/utils/get-url-from-post.js
+++ app/utils/get-url-from-post.js
@@ -1,11 +1,24 @@
 function getURLfromPost(posts) {
   const URLmatch = new RegExp("([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?([^ ])+")
-  const URLS = []
+  const URLs = []
+
+  function ignore(url){
+    let u = new URL(url);
+
+    const ignoredomains = [
+      "cdn.betterttv.net"
+    ];
+
+    return ignoredomains.some(domain => {
+      if (u.host.indexOf(domain) !== -1)
+        return true;
+    });
+  }
 
   posts.map(post => {
     return post.value.match(URLmatch)
   }).filter(item => {
-    if (item !== null) {
+    if (item !== null && !ignore(item[0])) {
       URLs.push(item[0])
     }
   })
