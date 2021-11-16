8435d59873005000781206bf818ec2b3db0715a2
Marco Wessel <marco@driebit.nl>
2021-11-16 12:57:18 +0100

    Adjust chat message styling

--- app/components/chat-msg.js
+++ app/components/chat-msg.js
@@ -5,14 +5,14 @@ const md = require('../utils/markdown')
 
 function chatMsg (msg) {
   return html`
-    <div class="x xdc xw pb1 ow">
-      <time datetime="${formatDate(msg.timestamp).iso}" class="ft-ms fs0-8">${formatDate(msg.timestamp).date}</time>
+    <div class="chat-message x xdc xw pb1 ow">
+      <time datetime="${formatDate(msg.timestamp).iso}" class="ft-ms fs0-8 fc-gr9">${formatDate(msg.timestamp).date}</time>
       <div class="pl1 copy">
-        <em>${msg.username}:</em>
+        <span class="fsi">${msg.username}:</span>
         ${raw(md(msg.value))}
       </div>
     </div>
-  ` 
+  `
 }
 
 module.exports = chatMsg
