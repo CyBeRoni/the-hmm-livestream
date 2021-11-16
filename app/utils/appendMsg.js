aff37fe8b3e889936142ef45be734ee1a299d368
Marco Wessel <marco@driebit.nl>
2021-11-16 13:05:32 +0100

    Scroll after loading any images
    to make sure the view is correctly scrolled to the full size of the element

--- app/utils/appendMsg.js
+++ app/utils/appendMsg.js
@@ -1,10 +1,26 @@
 const chatMsg = require('../components/chat-msg')
 
-function appendMsg (el, msg) {
+const loadImage = img =>
+  new Promise((resolve, reject) => {
+    img.onload = () => resolve(img);
+    img.onerror = reject;
+  });
+
+function appendMsg (el, msg, scroll = false) {
   const newMsg = chatMsg(msg)
 
   el.append(newMsg)
-  newMsg.scrollIntoView(false) 
+  if (scroll){
+    images = newMsg.getElementsByTagName("img");
+    if (images.length > 0){
+      i = Array.from(images);
+      Promise.allSettled(i.map(loadImage)).then(() => {
+        newMsg.scrollIntoView(false);
+      });
+    } else {
+      newMsg.scrollIntoView(false)
+    }
+  }
 }
 
 module.exports = appendMsg
